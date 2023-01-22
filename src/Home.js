4421610192adf425a6aefb319dcd3ad1e26b8578
santhiya-ramesh <santhiya.ramachandran@openturf.in>
2023-01-22 16:03:37 +0530

    Initial commit

--- src/Home.js
+++ src/Home.js
@@ -11,7 +11,7 @@ const Home = (props) =>{
       <Background>
         <View style={{marginHorizontal:30,marginVertical:260}}>
         <Text style={{color:'black',fontSize:22,fontWeight:'bold'}}>Welcome to Find Green</Text>
-        <Text style={{color:'black',fontSize:16,paddingVertical:5}}>Searching for electric vehicle charging stations in India? Visit  
+        <Text style={{color:'black',fontSize:16,paddingVertical:5}}>Searching for  electric vehicle charging stations in India? Visit  
         <Text style={styles.hyperlinkStyle} onPress={() => { Linking.openURL('https://techup.co.in');}}> Find Green</Text> and select your nearest location to get a list of fast charging stations for your electric vehicles.</Text>
         <Btn bgColor={green} textColor={'white'} btnLabel="Log In" Press={()=>props.navigation.navigate("Login")}/>
         <Btn bgColor='white' textColor={'black'} btnLabel="Register" Press={()=>props.navigation.navigate("Register")}/>
